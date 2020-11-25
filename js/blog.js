77a4ac1134081c509310e01f484da29beae9940c
Edib Supic <edib.supic@applicita.com>
2020-11-25 18:23:56 +0100

    blog two added

--- js/blog.js
+++ js/blog.js
@@ -2,21 +2,18 @@ function changeImage(image) {
     for (let im in images) {
         document.getElementById(im).style.border = 0;
     }
-    image.style.border = "2px solid darkcyan";
+    image.style.border = "5px solid darkcyan";
     document.getElementById("mainImage").src = images[image.id];
 }
 
 var images = {
-    "image1": "https://via.placeholder.com/900x600", 
-    "image2": "https://via.placeholder.com/900x600", 
-    "image3": "https://via.placeholder.com/900x600",
-    "image4": "https://via.placeholder.com/900x600", 
-    "image5": "https://via.placeholder.com/900x600", 
-    "image6": "https://via.placeholder.com/900x600",
-    "image7": "https://via.placeholder.com/900x600", 
-    "image8": "https://via.placeholder.com/900x600", 
-    "image9": "https://via.placeholder.com/900x600",
-    "image10": "https://via.placeholder.com/900x600", 
-    "image11": "https://via.placeholder.com/900x600", 
-    "image12": "https://via.placeholder.com/900x600"
+    "image1": "../image/blog/eloquent.png", 
+    "image2": "../image/blog/procsharp.png", 
+    "image3": "../image/blog/aspnet.png",
+    "image4": "../image/blog/react.png", 
+    "image5": "../image/blog/html5.png", 
+    "image6": "../image/blog/css.png",
+    "image7": "../image/blog/ydkjs.png", 
+    "image8": "../image/blog/design.png", 
+    "image9": "../image/blog/jwt.png"
 }
